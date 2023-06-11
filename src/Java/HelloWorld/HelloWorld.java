c56b5eeb8891bb1847fad5c12b7fe825af238597
Toubun <toubuntyan@gmail.com>
2023-06-11 23:15:03 +0800

    (05) Annotation

--- src/Java/HelloWorld/HelloWorld.java
+++ src/Java/HelloWorld/HelloWorld.java
@@ -1,6 +1,14 @@
 package Java.HelloWorld;
-
+//HelloWorldClass
 public class HelloWorld {
+    /*
+    test
+     */
+
+    /**
+     * main is method name
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World !");
     }

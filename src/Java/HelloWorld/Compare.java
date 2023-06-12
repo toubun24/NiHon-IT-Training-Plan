d3cea0fa579ee5f7f5e4d0792e5adb03a801cfe3
Toubun <toubuntyan@gmail.com>
2023-06-12 22:34:03 +0800

    (14) Bitwise operators

--- src/Java/HelloWorld/Compare.java
+++ src/Java/HelloWorld/Compare.java
@@ -2,6 +2,7 @@ package Java.HelloWorld;
 
 public class Compare {
     public static void main(String[] args) {
+        /*
         int a = 1;
         double b = 1.0;
         System.out.println(a == b);
@@ -10,5 +11,17 @@ public class Compare {
         System.out.println(a < b);
         System.out.println(a >= b);
         System.out.println(a <= b);
+        */
+
+        int a = 10, b = 20;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(Integer.toBinaryString(a&b));
+        System.out.println(Integer.toBinaryString(a|b));
+        System.out.println(Integer.toBinaryString(a^b));
+        System.out.println(~a);
+        System.out.println(Integer.toBinaryString(a<<2));
+        System.out.println(Integer.toBinaryString(a>>2));
+        System.out.println(Integer.toBinaryString(a>>>2));
     }
 }

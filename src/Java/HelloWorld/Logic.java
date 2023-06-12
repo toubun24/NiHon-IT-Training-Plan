ca7a8737b1bf7b0fd972f31d382d18f6dfe5e10d
Toubun <toubuntyan@gmail.com>
2023-06-12 22:20:05 +0800

    (12) Logical operators

---
+++ src/Java/HelloWorld/Logic.java
@@ -0,0 +1,9 @@
+package Java.HelloWorld;
+
+public class Logic {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        System.out.println(!(a==2)&&(b==2));
+        System.out.println((a==1)||(b==3));
+    }
+}

7bea353b5a96e39fcf31cacb9291c28e4ba9190f
Toubun <toubuntyan@gmail.com>
2023-06-13 00:32:52 +0800

    (20) DoWhile loop

---
+++ src/Java/HelloWorld/DoWhile01.java
@@ -0,0 +1,11 @@
+package Java.HelloWorld;
+
+public class DoWhile01 {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        }while (a == 10);
+    }
+}

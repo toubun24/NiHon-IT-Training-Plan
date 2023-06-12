badb9b409229d3015d791dbf20df9a04feb1da6b
Toubun <toubuntyan@gmail.com>
2023-06-12 22:24:28 +0800

    (13) Relational operators

---
+++ src/Java/HelloWorld/Compare.java
@@ -0,0 +1,14 @@
+package Java.HelloWorld;
+
+public class Compare {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 1.0;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+}

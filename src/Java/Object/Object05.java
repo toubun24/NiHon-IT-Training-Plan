ef1bde55f8cc7c5894dd7179fb4e3d5f77c24153
Toubun <toubuntyan@gmail.com>
2023-06-14 00:12:19 +0800

    (35) Method Constructor

--- src/Java/Object/Object05.java
+++ src/Java/Object/Object05.java
@@ -3,12 +3,33 @@ package Java.Object;
 import java.util.Arrays;
 
 public class Object05 {
+    String name;
+    int age;
+
+    public Object05() {
+
+    }
+
+    public Object05(String name) {
+        this.name = name;
+    }
+
+    public Object05(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public static void main(String[] args) {
         Object05 object04 = new Object05();
         object04.change("a", "b", "c", "d");
         object04.change(new String[]{"a", "b", "c", "d"});
 
         object04.change2(1, 2, "a", "b", "c", "d");
+    }
+
+    void print() {
+        System.out.println(name);
+        System.out.println(age);
     }
 
     void change(String... a) {

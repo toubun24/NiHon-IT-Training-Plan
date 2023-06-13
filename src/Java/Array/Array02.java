c1f10e55535afaae2aa7327179b00cda9a2eaa89
Toubun <toubuntyan@gmail.com>
2023-06-13 22:45:25 +0800

    (26) Two-dim array definition

---
+++ src/Java/Array/Array02.java
@@ -0,0 +1,19 @@
+package Java.Array;
+
+import java.util.Arrays;
+
+public class Array02 {
+    public static void main(String[] args) {
+        // int[][] arrayName = new int[5][];
+        int[][] arrayName = {{1, 2}, {3, 4}, {5}};
+        System.out.println(Arrays.deepToString(arrayName));
+
+        int[][] arrayName2 = new int[3][2];
+        arrayName2[0][0] = 1;
+        arrayName2[0][1] = 2;
+        arrayName2[1][0] = 3;
+        arrayName2[1][1] = 4;
+        arrayName2[2][0] = 5;
+        System.out.println(Arrays.deepToString(arrayName2));
+    }
+}

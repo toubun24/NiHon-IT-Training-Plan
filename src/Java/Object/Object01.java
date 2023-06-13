153500a217f8535b87f2d722d26ec7170289a3f6
Toubun <toubuntyan@gmail.com>
2023-06-13 22:58:25 +0800

    (29) Class definition

---
+++ src/Java/Object/Object01.java
@@ -0,0 +1,18 @@
+package Java.Object;
+
+public class Object01 {
+    class test {
+        String name;
+    }
+}
+
+class aaa{
+    int age;
+    void eat(){
+        System.out.println("eat");
+    }
+}
+
+class bbb{
+
+}

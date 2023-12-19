270b2796d577986117137fa1acc87d38029e1472
TangFenTyan <toubuntyan@gmail.com>
2023-12-19 15:22:08 +0800

    react-codes added by vscode

---
+++ react-codes/src/components/Hello/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className = 'title'>
+                Hello!
+            </div>
+        );
+    }
+}
+
+export default Hello;
---
+++ react-codes/src/components/ReactTest/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class ReactTest extends Component {
+    render() {
+        return (
+            <div className = 'div'>
+                React!
+            </div>
+        );
+    }
+}
+
+export default ReactTest;

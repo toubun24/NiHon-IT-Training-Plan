53c5bdb696a91a34eacb0f1a3b234af34295e187
TangFenTyan <toubuntyan@gmail.com>
2023-12-25 01:36:48 +0800

    code 12.25 01:35

--- React/react进阶/09_封装NavLink-src/components/MyNavLink/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-export default class MyNavLink extends Component {
-  render() {
-    return (
-        <NavLink className={({isActive}) => "list-group-item" + (isActive ? " light" : "")} {...this.props}/>
-    )
-  }
-}

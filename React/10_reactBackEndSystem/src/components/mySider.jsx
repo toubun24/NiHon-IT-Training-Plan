26f22b7c70381b8e104432f61b95b802bef2abf9
TangFenTyan <toubuntyan@gmail.com>
2024-01-12 18:54:31 +0800

    reactBES-侧边栏样式优化 01.12 18:55

--- React/10_reactBackEndSystem/src/components/mySider.jsx
+++ React/10_reactBackEndSystem/src/components/mySider.jsx
@@ -1,3 +1,6 @@
+// cd G:\NiHon-IT-Training-Plan\React\10_reactBackEndSystem\JSONServer
+// json-server db.json --port 5000
+
 import React, { useState, useEffect } from 'react'; // useState, useEffect
 import {
   UserOutlined,
@@ -7,7 +10,7 @@ import {
 } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
 import './mySider.less'
-import { useHistory } from 'umi'; // useHistory
+import { useHistory, useLocation } from 'umi'; // useHistory // useLocation
 import axios from 'axios'; // axios
 // import { responsiveArray } from 'antd/es/_util/responsiveObserver';
 
@@ -33,56 +36,88 @@ const items = [
   ]),
 ];
 */
+const getItem = (menuList) => {
+  return menuList.map(item => {
+    if (item.children && checkPagePermission(item) && item.children.length > 0) {
+      return {
+        key: item.key,
+        label: item.label,
+        children: getItem(item.children), // .map is not a function => .children
+        icon: iconList[item.key], // icon // []
+      }
+    }
+    return checkPagePermission(item) && {
+      key: item.key,
+      label: item.label,
+      icon: iconList[item.key], // icon // []
+    }
+  })
+}
+const checkPagePermission = (item) => {
+  return item.pagepermission === 1 // true/false
+}
+const iconList = {
+  '/home': <AimOutlined />,
+  '/user-manage': <UserOutlined />,
+  '/user-manage/list': <UnorderedListOutlined />,
+  '/right-manage': <AuditOutlined />,
+  '/right-manage/role/list': <UnorderedListOutlined />,
+  '/right-manage/right/list': <UnorderedListOutlined />,
+}
 
 const MySider = ({ collapsed }) => { // {collapsed}
   const { token: { }, } = theme.useToken();
   const history = useHistory(); // useHistory
   const [menuList, setMenuList] = useState([]) // menuList
+  const location = useLocation() // useLocation
   useEffect(
     () => {
-axios.get('http://localhost:5000/rights?_embed=children').then(
-  response=>{
-    console.log(response.data)
-    setMenuList(response.data)
-  }
-)
-    },[]
+      axios.get('http://localhost:5000/rights?_embed=children').then(
+        response => {
+          console.log(response.data)
+          setMenuList(response.data)
+        }
+      )
+    }, []
   )
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
-      <div className="demo-logo-vertical">
-        后台系统
+      <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+        <div className="demo-logo-vertical">后台系统</div>
+        <div style={{ flex: 1, overflow: 'auto' }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            // defaultSelectedKeys={['1']} // '/home'
+            selectedKeys={[location.pathname]} // 当前选中的菜单项key数组 // location.pathname
+            /*
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+            */
+            // defaultOpenKeys={['sub1']} // menu
+            defaultOpenKeys={['/' + location.pathname.split('/')[1]]} // {[]} // 截取
+            // items={items} // menu
+            // items={menuList} // menuList
+            items={getItem(menuList)}
+            onClick={({ key }) => { history.push(key) }} // history // function({ item, key, keyPath, domEvent })
+          />
+        </div>
       </div>
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        /*
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        */
-        defaultOpenKeys={['sub1']} // menu
-        // items={items} // menu
-        items={menuList} // menuList
-        onClick={({ key }) => {
-          history.push(key) // history
-        }} // function({ item, key, keyPath, domEvent })
-      />
     </Sider>
   );
 };
